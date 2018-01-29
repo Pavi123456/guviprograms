8deab8dc8332ba8a0f4b9958a785c0f79db31751
karthikeyan638182 <30829996+karthikeyan638182@users.noreply.github.com>
2018-01-29 19:26:12 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("Enter the value of a: ");
+scanf("%d",&a);
+printf("Enter the value of b: ");
+scanf("%d",&b);
+printf("Enter the value of c: ");
+scanf("%d",&c);
+c=a+b;
+printf("\nThe addition value is:%d",c);
+}

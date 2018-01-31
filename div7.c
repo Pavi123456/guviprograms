ec1b502294dabdde240812855e4ca6a11724501a
karthikeyan638182 <30829996+karthikeyan638182@users.noreply.github.com>
2018-01-31 20:49:23 +0530

    Create div7.c

---
+++ div7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter the value of n");
+    scanf("%d",&n);
+    if(n%7==0)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}

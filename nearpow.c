1152d08b4b5aa62950956708e789a6a8645216fd
karthikeyan638182 <30829996+karthikeyan638182@users.noreply.github.com>
2018-01-31 20:59:14 +0530

    Create nearpow.c

---
+++ nearpow.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int  main()
+{
+	int n,i,c=0,m=1,p=2;
+	scanf("%d",&n);
+	while(n!=1)
+	{
+		n=n/2;
+	c++;
+	}
+	for(i=0;i<c+1;i++)
+	{
+		m=m*p;
+	}
+	printf("%d",m);
+  return 0;
+}

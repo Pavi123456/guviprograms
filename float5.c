6d201c14c721d6a0e849ed45c5bbac419e4c7394
karthikeyan638182 <30829996+karthikeyan638182@users.noreply.github.com>
2018-02-06 10:42:53 +0000

    Create float5.c

---
+++ float5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    float l,b,a;
+    printf("Enter the length ");
+    scanf("%f",&l);
+    printf("\nEnter the breadth");
+    scanf("%f",&b);
+    a=l*b;
+    printf("\nThe Area of field is %.5f",a);
+}

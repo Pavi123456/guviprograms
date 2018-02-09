e8c283cb200cce2548dbc440efda6b93a1d2b5b0
karthikeyan638182 <30829996+karthikeyan638182@users.noreply.github.com>
2018-02-09 14:13:49 +0530

    Create celsius.c

---
+++ celsius.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    float celsius,kelvin;
+    printf("Enter the celsius");
+    scanf("%f",&celsius);
+    kelvin=celsius+273.15;
+    printf("\nkelvin is %f",kelvin);
+}

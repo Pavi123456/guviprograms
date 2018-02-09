8b36d1ca48fbd0aaa22c192a8974d57496fc2199
karthikeyan638182 <30829996+karthikeyan638182@users.noreply.github.com>
2018-02-09 14:57:46 +0530

    Create proint.c

---
+++ proint.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,ans=1,r;
+printf("Enter the number");
+scanf("%d",&n);
+while(n!=0)
+{
+r=n%10;
+ans=ans*r;
+n=n/10;
+}
+printf("the product is %d",ans);
+return 0;
+}

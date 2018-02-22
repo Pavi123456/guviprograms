8e45770c58f1b80179ed60984e089537a030a04c
karthikeyan638182 <30829996+karthikeyan638182@users.noreply.github.com>
2018-02-22 16:48:48 +0530

    Update and rename island.c to p33.c

--- p33.c
+++ p33.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int a[100][100],m,n,i,j,c=0;
+	int b[100][100],m,n,i,j,c=0;
 	printf("\nenter the row size : ");
 	scanf("%d",&m);
 	printf("\nenter the column size : ");
@@ -11,16 +11,16 @@ int main()
 	{
 	    for(j=1;j<=n;j++)
 		{
-			scanf("%d",&a[i][j]);
+			scanf("%d",&b[i][j]);
 		}
 	}
 	for(i=1;i<=m;i++)
 	{
 		for(j=1;j<=n;j++)
 		{
-		    if(a[i][j]==1)
+		    if(b[i][j]==1)
 			{
-				if(a[i-1][j-1]==0&&a[i][j+1]==0&&a[i][j-1]==0&&a[i+1][j+1]==0)
+				if(b[i-1][j-1]==0&&b[i][j+1]==0&&b[i][j-1]==0&&b[i+1][j+1]==0)
 				{
 					c=c+1;
 				}

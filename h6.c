8734b74d2b00e5843286c912ed75c92562412673
karthikeyan638182 <30829996+karthikeyan638182@users.noreply.github.com>
2018-02-27 16:21:36 +0530

    Create h6.c

---
+++ h6.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,j,a[1000],count=0,t;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+	scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+for(j=0;j<n;j++)
+{
+	if(a[i]==a[j])
+	{
+		t=a[j];
+		count=j;
+	}
+}
+	if(count!=0)
+	{
+	printf("%d",t);
+	}
+	else
+	{
+	    printf("Unique");
+	}
+}

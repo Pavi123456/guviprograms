794a05bda63395e0e77e80ec0e62b9d56d225a05
karthikeyan638182 <30829996+karthikeyan638182@users.noreply.github.com>
2018-02-27 15:40:42 +0530

    Create 109.c

---
+++ 109.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int a[20],j,i,t;
+    printf("Enter the array elements");
+    for(i-0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+        for(j=i+1;j<10;j++)
+        {
+            if(a[i]>a[j])
+            {
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+    }
+    printf("The minimum value is :%d",a[0]);
+}

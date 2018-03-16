f3aab19172ab51f1fa602610531b750aec4a6ab8
karthikeyan638182 <30829996+karthikeyan638182@users.noreply.github.com>
2018-03-16 14:50:46 +0530

    Create p51.c

---
+++ p51.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+    int a[100],n,i,j,t;
+    printf("Enter the array size");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+    }
+    printf("%d",a[1]);
+}
